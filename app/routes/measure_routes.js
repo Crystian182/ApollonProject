93fecefe7e99ff88616d693046d72f2f54f719c8
crystian182 <cristian.vergallo@gmail.com>
2019-03-18 13:31:03 +0100

    GetAll measure method

--- app/routes/measure_routes.js
+++ app/routes/measure_routes.js
@@ -3,39 +3,49 @@ module.exports = function(app, db) {
     var ObjectID = require('mongodb').ObjectID;
 
     app.get('/measures/:id', (req, res) => {
-        const id = req.params.id;
-        const details = { '_id': new ObjectID(id) };
-        db.collection('measures').findOne(details, (err, item) => {
-          if (err) {
-            res.send({'error':'An error has occurred'});
-          } else {
-            res.send(item);
-          } 
-        });
+      const id = req.params.id;
+      const details = { '_id': new ObjectID(id) };
+      db.collection('measures').findOne(details, (err, item) => {
+        if (err) {
+          res.send({'error':'An error has occurred'});
+        } else {
+          res.send(item);
+        } 
       });
+    });
+
+    app.get('/measures', (req, res) => {
+      db.collection('measures').find({}).toArray(function (err, result) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(JSON.stringify(result));
+        }
+      })
+    });
 
     app.post('/measures', (req, res) => {
-        const measure = { text: req.body.text, title: req.body.title };
-        db.collection('measures').insert(measure, (err, result) => {
-          if (err) { 
-            res.send({ 'error': 'An error has occurred' }); 
-          } else {
-            res.send(result.ops[0]);
-          }
-        });
+      const measure = { text: req.body.text, title: req.body.title };
+      db.collection('measures').insert(measure, (err, result) => {
+        if (err) { 
+          res.send({ 'error': 'An error has occurred' }); 
+        } else {
+          res.send(result.ops[0]);
+        }
       });
+    });
 
-      app.delete('/measures/:id', (req, res) => {
-        const id = req.params.id;
-        const details = { '_id': new ObjectID(id) };
-        db.collection('measures').remove(details, (err, item) => {
-          if (err) {
-            res.send({'error':'An error has occurred'});
-          } else {
-            res.send('Measure ' + id + ' deleted!');
-          } 
-        });
+    app.delete('/measures/:id', (req, res) => {
+      const id = req.params.id;
+      const details = { '_id': new ObjectID(id) };
+      db.collection('measures').remove(details, (err, item) => {
+        if (err) {
+          res.send({'error':'An error has occurred'});
+        } else {
+          res.send('Measure ' + id + ' deleted!');
+        } 
       });
+    });
 
       app.put('/measures/:id', (req, res) => {
         const id = req.params.id;
