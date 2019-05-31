dacbeb7acea7695c00da404af1049ee125801f20
crystian182 <cristian.vergallo@gmail.com>
2019-05-31 09:23:25 +0200

    Chron + d3 rest service

--- server.js
+++ server.js
@@ -7,17 +7,23 @@ const MongoClient     = require('mongodb').MongoClient;
 const url             = 'mongodb://localhost:27017/apollonproject'
 const port            = 8000;
 
+var jobs              = require('./app/jobs/dbtransfer.js');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
 MongoClient.connect(url, (err, database) => {
   if (err) return console.log(err)
-                      
+
   require('./app/routes')(app, database.db("apollonproject"));
   app.listen(port, () => {
     console.log('MongoDB live on port ' + port);
-  });               
+  });  
+  
+  jobs.processDataFromMongo(app, database.db("apollonproject"));
 })
+
+
 
   
