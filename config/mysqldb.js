a2074f571463d59c40a9f26ba7fe3b0d2f7b2ee2
robfas <39085848+robfas@users.noreply.github.com>
2019-05-31 09:24:35 +0200

    mysql

--- config/mysqldb.js
+++ config/mysqldb.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: 'root',
-    database: 'crudb',
+    database: 'apollonprojectdb',
     dateStrings: [
         'DATE',
         'DATETIME'
